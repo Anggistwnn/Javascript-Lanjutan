99f9e41e1ff11644d9c23375bd7a9142eba0a74c
Anggistwnn <Anggistwnn@gmail.com>
2022-04-27 17:07:04 +0700

    update  - construction function

--- script.js
+++ script.js
@@ -19,22 +19,42 @@
 // }
 
 // 2. function declaration
-function Mahasiswa(nama, energi) {
-    let mahasiswa = {};
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
 
-    mahasiswa.makan = function (porsi) {
+//     mahasiswa.makan = function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Halo ${this.nama},   Selamat Makan ! `);
+//     }
+//     mahasiswa.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, Selamat Bermain`);
+//     }
+
+//     return mahasiswa;    
+// }
+
+// let Anggi = Mahasiswa ('Anggi', 10); 
+// let AdeWaliya = Mahasiswa ('Ade Waliya', 10);
+
+// 3. construction function
+// keyword new
+
+function Mahasiswa(nama, energi) {
+
+    this.nama = nama;
+    this.energi = energi;
+
+    this.makan = function (porsi) {
         this.energi = this.energi + porsi;
         console.log(`Halo ${this.nama},   Selamat Makan ! `);
     }
-    mahasiswa.main = function (jam) {
+    this.main = function (jam) {
         this.energi -= jam;
         console.log(`Halo ${this.nama}, Selamat Bermain`);
     }
-
-    return mahasiswa;    
 }
 
-let Anggi = Mahasiswa ('Anggi', 10); 
-let AdeWaliya = Mahasiswa ('Ade Waliya', 10);
+let anggi = new Mahasiswa ('anggi', 20);

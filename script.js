6d7cc41694cef4b1f9047d7c27c4c90a67831888
Anggistwnn <Anggistwnn@gmail.com>
2022-05-04 15:19:39 +0700

    update

--- script.js
+++ script.js
@@ -19,22 +19,22 @@
 // }
 
 // 2. function declaration
-// function Mahasiswa(nama, energi) {
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
+function Mahasiswa(nama, energi) {
+    let mahasiswa = {};
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
 
-//     mahasiswa.makan = function (porsi) {
-//         this.energi = this.energi + porsi;
-//         console.log(`Halo ${this.nama},   Selamat Makan ! `);
-//     }
-//     mahasiswa.main = function (jam) {
-//         this.energi -= jam;
-//         console.log(`Halo ${this.nama}, Selamat Bermain`);
-//     }
+    mahasiswa.makan = function (porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Halo ${this.nama},   Selamat Makan ! `);
+    }
+    mahasiswa.main = function (jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, Selamat Bermain`);
+    }
 
-//     return mahasiswa;    
-// }
+    return mahasiswa;    
+}
 
 // let Anggi = Mahasiswa ('Anggi', 10); 
 // let AdeWaliya = Mahasiswa ('Ade Waliya', 10);
@@ -42,19 +42,19 @@
 // 3. construction function
 // keyword new
 
-function Mahasiswa(nama, energi) {
+// function Mahasiswa(nama, energi) {
 
-    this.nama = nama;
-    this.energi = energi;
+//     this.nama = nama;
+//     this.energi = energi;
 
-    this.makan = function (porsi) {
-        this.energi = this.energi + porsi;
-        console.log(`Halo ${this.nama},   Selamat Makan ! `);
-    }
-    this.main = function (jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, Selamat Bermain`);
-    }
-}
+//     this.makan = function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Halo ${this.nama},   Selamat Makan ! `);
+//     }
+//     this.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, Selamat Bermain`);
+//     }
+// }
 
-let anggi = new Mahasiswa ('anggi', 30);
+// let anggi = new Mahasiswa ('anggi', 30);

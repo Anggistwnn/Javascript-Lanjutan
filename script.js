8205c2248a9ee13faa5a9a0a599cc3180348c785
Anggistwnn <Anggistwnn@gmail.com>
2022-05-04 15:24:04 +0700

    add quokka extension in file

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 // Cara membuat object pada javascript
 // 1. Object Literal
-// let mahasiswa1 = {
-//     nama: 'Anggi Setiawan',
-//     energi: 10,
-//     makan: function (porsi) {
-//         this.energi = this.energi + porsi;
-//         console.log(`Hallo ${this.nama} Selamat makan !`);
-//     }
-// }
+let mahasiswa1 = {
+    nama: 'Anggi Setiawan',
+    energi: 10,
+    makan: function (porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Hallo ${this.nama} Selamat makan !`);
+    }
+}
 
-// let mahasiswa2 = {
-//     nama: 'Ade Waliya',
-//     energi: 10,
-//     makan: function (porsi) {
-//         this.energi = this.energi + porsi;
-//         console.log(`Hallo ${this.nama} Selamat makan !`);
-//     }
-// }
+let mahasiswa2 = {
+    nama: 'Ade Waliya',
+    energi: 10,
+    makan: function (porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Hallo ${this.nama} Selamat makan !`);
+    }
+}
 
 // 2. function declaration
-function Mahasiswa(nama, energi) {
-    let mahasiswa = {};
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
 
-    mahasiswa.makan = function (porsi) {
-        this.energi = this.energi + porsi;
-        console.log(`Halo ${this.nama},   Selamat Makan ! `);
-    }
-    mahasiswa.main = function (jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, Selamat Bermain`);
-    }
+//     mahasiswa.makan = function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Halo ${this.nama},   Selamat Makan ! `);
+//     }
+//     mahasiswa.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, Selamat Bermain`);
+//     }
 
-    return mahasiswa;    
-}
+//     return mahasiswa;    
+// }
 
 // let Anggi = Mahasiswa ('Anggi', 10); 
 // let AdeWaliya = Mahasiswa ('Ade Waliya', 10);
---
+++ scriptQuokka.js
@@ -0,0 +1,5 @@
+const angka1 = 10;
+const angka2 = 1;
+
+hasil = angka1 + angka2;
+console.log(hasil);

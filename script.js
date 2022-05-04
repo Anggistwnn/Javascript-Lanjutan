a356f2b575b8cc0282d17ecec26d2473b06671f0
Anggistwnn <Anggistwnn@gmail.com>
2022-05-04 15:47:18 +0700

    update qoukka tips

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 // Cara membuat object pada javascript
-// 1. Object Literal
-let mahasiswa1 = {
-    nama: 'Anggi Setiawan',
-    energi: 10,
-    makan: function (porsi) {
-        this.energi = this.energi + porsi;
-        console.log(`Hallo ${this.nama} Selamat makan !`);
-    }
-}
+// 1. Object Literal (tidak efektiv untuk object yang baanyak)
+// let mahasiswa1 = {
+//     nama: 'Anggi Setiawan',
+//     energi: 10,
+//     makan: function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Hallo ${this.nama} Selamat makan !`);
+//     }
+// }
 
-let mahasiswa2 = {
-    nama: 'Ade Waliya',
-    energi: 10,
-    makan: function (porsi) {
-        this.energi = this.energi + porsi;
-        console.log(`Hallo ${this.nama} Selamat makan !`);
-    }
-}
+// let mahasiswa2 = {
+//     nama: 'Ade Waliya',
+//     energi: 10,
+//     makan: function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Hallo ${this.nama} Selamat makan !`);
+//     }
+// }
+
+
 
 // 2. function declaration
 // function Mahasiswa(nama, energi) {
--- scriptQuokka.js
+++ scriptQuokka.js
@@ -1,5 +1,42 @@
+// penjumlahan sederhana
 const angka1 = 10;
 const angka2 = 1;
-
 hasil = angka1 + angka2;
 console.log(hasil);
+// membuat object data sederhana
+const obj = {
+    Nama : 'anggi',
+    Alamat : 'Pamulang',
+    Umur : 22
+}
+console.log(obj);
+console.log(obj.Nama);  
+console.log(obj.Alamat);
+console.log(obj.Umur);
+// membuat arrya menggunakan quoka
+ 
+ const angka = ['1','2','3','4'];
+ angka
+ console.log(angka[3]);
+
+//  membuat pengkondisian
+const biodata = {
+    Nama : 'anggi',
+    Alamat : 'Pamulang',
+    Umur : 1
+}
+
+if (biodata.Umur < 20) {
+    console.log('Remaja');
+} else if (biodata.Umur > 20){
+    console.log('Dewasa');
+}
+
+// membuat function
+
+function perhitungan(a,b) {
+    return a+b;
+}
+
+const result = perhitungan(40,20);
+result

294791e75393418a685663ef3f32ca7ed8ab470b
Anggistwnn <Anggistwnn@gmail.com>
2022-05-06 11:05:20 +0700

    update - prototype for object makes it esier

--- script.js
+++ script.js
@@ -22,40 +22,73 @@
 
 // 2. function declaration
 
-const methodMahasiswa = {
-    makan: function (porsi) {
-        this.energi = this.energi + porsi;
-        console.log(`Halo ${this.nama},   Selamat Makan ! `);
-    },
-    main: function (jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, Selamat Bermain`);
-    },
+// const methodMahasiswa = {
+//     makan: function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Halo ${this.nama},   Selamat Makan ! `);
+//     },
+//     main: function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, Selamat Bermain`);
+//     },
 
-    tidur: function (jam) {
-        this.energi += jam * 2;
-        console.log(`Halo ${this.nama}, selaamt tidur!`);
-    }
-}
+//     tidur: function (jam) {
+//         this.energi += jam * 2;
+//         console.log(`Halo ${this.nama}, selaamt tidur!`);
+//     }
+// }
+
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+
+//     return mahasiswa;    
+// }
+
+// let Anggi = Mahasiswa ('Setiawan', 10); 
+
+//  console.log(Anggi.tidur(5));
+//  Anggi
+//  console.log(Anggi.main(2));
+//  Anggi
+// console.log(Anggi.main(10));
+// Anggi
+// console.log(Anggi.tidur(30));
+// Anggi
+
 
 function Mahasiswa(nama, energi) {
-    let mahasiswa = Object.create(methodMahasiswa);
-    mahasiswa.nama = nama;
-    mahasiswa.energi = energi;
-
-    return mahasiswa;    
+    //let mahasiswa = Object.create(methodMahasiswa);
+    // let mahasiswa = {}
+    this.nama = nama;
+    this.energi = energi;
+}
+Mahasiswa.prototype.makan = function (porsi) {
+  this.energi += porsi;
+  return `Halo ${this.nama}, Selamat Makan!`;  
+}
+Mahasiswa.prototype.main = function (main) {
+    this.energi -= main;
+    return;
 }
 
-let Anggi = Mahasiswa ('Setiawan', 10); 
+let Anggi = new Mahasiswa ('Anggi', 20);
+console.log(Anggi);
+console.log(Anggi.makan(3));
+console.log(Anggi);
+console.log(Anggi.main(2));
+console.log(Anggi);
 
- console.log(Anggi.tidur(5));
- Anggi
- console.log(Anggi.main(2));
- Anggi
-console.log(Anggi.main(10));
-Anggi
-console.log(Anggi.tidur(30));
-Anggi
+
+
+
+
+
+    
+
+
+
 // 3. construction function
 // keyword new
 

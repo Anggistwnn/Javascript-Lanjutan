0b1ce2ce08db5ec1f0fe799250eb07115cffaecc
Anggistwnn <Anggistwnn@gmail.com>
2022-05-18 09:00:45 +0700

    update

--- script.js
+++ script.js
@@ -411,3 +411,17 @@ const mhs1 = {
 // }
 //     const Anggistwnn = new Mahasiswa();
 
+const box = document.querySelector('.box');
+box.addEventListener('click', function () {
+    let satu = 'size';
+    let dua = 'caption';
+
+    if(this.classList.contains(satu)){
+         [satu, dua] = [dua, satu];
+    }
+
+    this.classList.toggle(satu);
+    setTimeout( () => {
+    this.classList.toggle(dua);
+    }, 600);
+});

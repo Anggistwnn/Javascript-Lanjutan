6c1a6f53e640f4c9d6a3d766800332aa34e750fb
Anggistwnn <Anggistwnn@gmail.com>
2022-05-19 10:12:24 +0700

    update

--- script.js
+++ script.js
@@ -453,10 +453,14 @@ const mhs1 = {
 //     console.log('tombol ditekan!');
 // })
 
-let total = 0,
-    count = 1;
-while (count <= 6) {
-    total += count;
-    count += 1;
+const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
+//  mencari angka >= 3
+// for
+const newAngka = [];
+for (let i = 0; i < angka.length; i++) {
+    if (angka[i] >= 3) {
+        newAngka.push(angka[i]);
+    }
 }
-console.log(total);
+
+console.log(newAngka);

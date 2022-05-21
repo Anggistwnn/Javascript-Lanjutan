495f2edf81bedf145c1e30c6e7cd4e0baf75af84
Anggistwnn <Anggistwnn@gmail.com>
2022-05-21 18:52:45 +0700

    update

--- script.js
+++ script.js
@@ -494,7 +494,7 @@ const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
 // console.log(hasil);
 // ---------------------------------------------------------------
 // ambil semua elemen video
-const videos = document.querySelectorAll('[data-duration]');
+const allVideos = document.querySelectorAll('[data-duration]');
 console.log(videos);
 // pilih hanya yang javascript lanjutan
 const video = document.querySelectorAll('');

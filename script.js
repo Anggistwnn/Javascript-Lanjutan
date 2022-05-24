53ddceff910af58db206a670ed143c3d504f70e5
Anggistwnn <Anggistwnn@gmail.com>
2022-05-24 07:22:21 +0700

    update

--- script.js
+++ script.js
@@ -504,8 +504,8 @@ const jsLanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT L
     // ubah durasi menjadi int, ubah menit menjadi detik
     .map(waktu => {
         // 10: 30[10, 30] split
-        const parts = waktu.split(':');
-        return parts;
+        const parts = waktu.split(':').map(part => parseFloat(part));
+        return (parts[0] * 60 + parts[1]);
     })
 // jumlahkan semua detik
 

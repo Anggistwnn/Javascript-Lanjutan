165ca8a8143159e2ac13a0d23484e01ae6a86ecf
Anggistwnn <Anggistwnn@gmail.com>
2022-05-24 07:44:49 +0700

    update

--- script.js
+++ script.js
@@ -497,19 +497,40 @@
 const videos = Array.from(document.querySelectorAll('[data-duration]'));
 // pilih hanya yang javascript lanjutan
 
-const jsLanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
+let jsLanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
 
     // ambil durasi masing-masing video
     .map(item => item.dataset.duration)
+
+
     // ubah durasi menjadi int, ubah menit menjadi detik
     .map(waktu => {
         // 10: 30[10, 30] split
         const parts = waktu.split(':').map(part => parseFloat(part));
         return (parts[0] * 60 + parts[1]);
     })
-// jumlahkan semua detik
+
+    // jumlahkan semua detik
+    .reduce((total, detik) => total + detik);
+
 
 // ubah format jadi jam menit detik
+const jam = Math.floor(jsLanjut / 3600);
+jsLanjut = jsLanjut - jam * 3600;
+const menit = Math.floor(jsLanjut / 60);
+const detik = jsLanjut - menit * 60;
+
 
 // simpan di DOM
-console.log(jsLanjut);
+const pDurasi = document.querySelector('.total-durasi');
+pDurasi.textContent = `${jam} Jam, ${menit} Menit, ${detik} Detik.`
+
+// Jumlah Video
+const jmlVideo = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN')).length;
+const pJmlVideo = document.querySelector('.jumlah-video');
+pJmlVideo.textContent = `${jmlVideo} Video.`
+
+
+
+
+console.log(jmlVideo);

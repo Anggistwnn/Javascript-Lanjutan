189043ca273add8b612870649cd305a73317104d
Anggistwnn <Anggistwnn@gmail.com>
2022-05-24 23:17:36 +0700

    update

--- script.js
+++ script.js
@@ -494,39 +494,39 @@
 // console.log(hasil);
 // ---------------------------------------------------------------
 // ambil semua elemen video
-const videos = Array.from(document.querySelectorAll('[data-duration]'));
+// const videos = Array.from(document.querySelectorAll('[data-duration]'));
 // pilih hanya yang javascript lanjutan
 
-let jsLanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
+// let jsLanjut = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN'))
 
-    // ambil durasi masing-masing video
-    .map(item => item.dataset.duration)
+// ambil durasi masing-masing video
+// .map(item => item.dataset.duration)
 
 
-    // ubah durasi menjadi int, ubah menit menjadi detik
-    .map(waktu => {
-        // 10: 30[10, 30] split
-        const parts = waktu.split(':').map(part => parseFloat(part));
-        return (parts[0] * 60 + parts[1]);
-    })
+// ubah durasi menjadi int, ubah menit menjadi detik
+// .map(waktu => {
+// 10: 30[10, 30] split
+//     const parts = waktu.split(':').map(part => parseFloat(part));
+//     return (parts[0] * 60 + parts[1]);
+// })
 
-    // jumlahkan semua detik
-    .reduce((total, detik) => total + detik);
+// jumlahkan semua detik
+// .reduce((total, detik) => total + detik);
 
 
 // ubah format jadi jam menit detik
-const jam = Math.floor(jsLanjut / 3600);
-jsLanjut = jsLanjut - jam * 3600;
-const menit = Math.floor(jsLanjut / 60);
-const detik = jsLanjut - menit * 60;
+// const jam = Math.floor(jsLanjut / 3600);
+// jsLanjut = jsLanjut - jam * 3600;
+// const menit = Math.floor(jsLanjut / 60);
+// const detik = jsLanjut - menit * 60;
 
 
 // simpan di DOM
-const pDurasi = document.querySelector('.total-durasi');
-pDurasi.textContent = `${jam} Jam, ${menit} Menit, ${detik} Detik.`
+// const pDurasi = document.querySelector('.total-durasi');
+// pDurasi.textContent = `${jam} Jam, ${menit} Menit, ${detik} Detik.`
 
 // Jumlah Video
-const jmlVideo = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN')).length;
-const pJmlVideo = document.querySelector('.jumlah-video');
-pJmlVideo.textContent = `${jmlVideo} Video.`
-console.log(jmlVideo);
+// const jmlVideo = videos.filter(video => video.textContent.includes('JAVASCRIPT LANJUTAN')).length;
+// const pJmlVideo = document.querySelector('.jumlah-video');
+// pJmlVideo.textContent = `${jmlVideo} Video.`
+// console.log(jmlVideo);

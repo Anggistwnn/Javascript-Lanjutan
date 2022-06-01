1e94445cf149a9fabe70897185cda72d79ff0ea8
Anggistwnn <Anggistwnn@gmail.com>
2022-06-01 08:57:31 +0700

    update

--- script.js
+++ script.js
@@ -780,8 +780,8 @@
 function penjumlahanPerkalian(a, b) {
     return [a + b, a * b];
 }
-
-const jumlah = penjumlahanPerkalian(2, 3)[1];
-
-const kali = penjumlahanPerkalian(2, 3)[1];
+// const jumlah = penjumlahanPerkalian(2, 3)[1];
+// const kali = penjumlahanPerkalian(2, 3)[1];
+const [jumlah, kali] = penjumlahanPerkalian(2, 3);
 console.log(jumlah);
+console.log(kali);
--- scriptQuokka.js
+++ scriptQuokka.js
@@ -5,38 +5,38 @@ hasil = angka1 + angka2;
 console.log(hasil);
 // membuat object data sederhana
 const obj = {
-    Nama : 'anggi',
-    Alamat : 'Pamulang',
-    Umur : 22
+    Nama: 'anggi',
+    Alamat: 'Pamulang',
+    Umur: 22
 }
 console.log(obj);
-console.log(obj.Nama);  
+console.log(obj.Nama);
 console.log(obj.Alamat);
 console.log(obj.Umur);
 // membuat arrya menggunakan quoka
- 
- const angka = ['1','2','3','4'];
- angka
- console.log(angka[3]);
+
+const angka = ['1', '2', '3', '4'];
+
+console.log(angka[3]);
 
 //  membuat pengkondisian
 const biodata = {
-    Nama : 'anggi',
-    Alamat : 'Pamulang',
-    Umur : 1
+    Nama: 'anggi',
+    Alamat: 'Pamulang',
+    Umur: 1
 }
 
 if (biodata.Umur < 20) {
     console.log('Remaja');
-} else if (biodata.Umur > 20){
+} else if (biodata.Umur > 20) {
     console.log('Dewasa');
 }
 
 // membuat function
 
-function perhitungan(a,b) {
-    return a+b;
+function perhitungan(a, b) {
+    return a + b;
 }
 
-const result = perhitungan(40,20);
+const result = perhitungan(40, 20);
 result
